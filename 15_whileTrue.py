e1aaed9aa671e18e57ce4a792f28fd50fed4ab49
edgarllano <76897642+edgarllano@users.noreply.github.com>
2021-02-08 13:52:54 -0500

    Add files via upload

---
+++ 15_whileTrue.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 25 15:24:52 2021
+
+@author: DELL
+"""
+
+x=input('Ingrese el # que contare ')
+x=int(x)
+y=1
+while True:
+    print(y)
+    y+=1
+    if y>x:
+       break
