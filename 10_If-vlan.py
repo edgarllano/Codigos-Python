8d3480044b3a3331cca324c91bb210d9e17760fa
edgarllano <76897642+edgarllano@users.noreply.github.com>
2021-02-08 13:40:01 -0500

    Add files via upload

---
+++ 10_If-vlan.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  8 13:37:51 2021
+
+@author: DELL
+"""
+
+datos = 10
+nativa = 100
+if datos == nativa:
+    print("Las VLAN son iguales.")
+else:
+    print("Las VLAN son diferentes.")
