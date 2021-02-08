c8076a340032c1a838f4b9f3ede3938a147e8435
edgarllano <76897642+edgarllano@users.noreply.github.com>
2021-02-08 13:47:59 -0500

    Add files via upload

---
+++ 13_forrange.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 25 15:14:46 2021
+
+@author: DELL
+"""
+
+print("Comienzo")
+for i in range(10,0,-1):
+    print(i, end=" ")
+print()
+print("Final")
