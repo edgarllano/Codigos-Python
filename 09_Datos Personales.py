deaa8aa5f4dcf36ccaba45a3ee01891f153e5a39
edgarllano <76897642+edgarllano@users.noreply.github.com>
2021-02-08 13:31:09 -0500

    Add files via upload

---
+++ 09_Datos Personales.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 18 15:12:48 2021
+
+@author: DELL
+"""
+
+nom=input('Ingrese su Nombre:')
+ape=input('Ingrese su Apellido:')
+dirr=input('Ingrese su Dirección:')
+eda=input('Ingrese su Edad:')
+
+print('hola!',nom,ape,',sistema refleja los datos Edad:',eda,
+     ',Direccion Domicilio:',dirr,',se encuentra registrado en el sistema',
+      '.Ha sido notificado por este medio.' )
