5b1966f4f2a92c513861ff5a1c37a2c202a8acfd
edgarllano <76897642+edgarllano@users.noreply.github.com>
2021-02-08 12:57:43 -0500

    Add files via upload

---
+++ 06_Dictionaries.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+dict1={'R1':'10.0.0.1',
+       1:'AP',
+       2:2.5,3:True,
+       'R1':'172.17.0.1'}
+print(dict1)
+print(len(dict1))
+print(type(dict1))
+print(dict1[1])
+print(dict1[2])
+print(dict1[3])
+print(dict1['R1'])
+dict1['R3']='10.0.0.3'
+print('R3' in dict1)
